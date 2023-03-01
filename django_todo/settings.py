21e1e831c54e01a8b298575da66577cb3b3a99e3
lpewton <liampewton99@gmail.com>
2023-03-01 02:43:06 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m(tgy9ev=yi7l$x)#)$i)-@von(xzadwn6xrghrvs^-i@kg9#1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lpewton-django-to-do-app.herokuapp.com']
 
 
 # Application definition
